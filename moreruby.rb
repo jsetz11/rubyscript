22a451e27a8b9226b57193086f04f8b41c6d82a5
jsetz11 <jsetz@suse.com>
2017-04-18 15:25:30 +0200

    New Version with SFTP

--- moreruby.rb
+++ moreruby.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/ruby
 # coding: utf-8
-require 'optparse'
 require 'fileutils'
 require 'getoptlong'
+require 'net/sftp'
+require 'rubygems'
 
 def argument
   opts = GetoptLong.new(
-    [ '--help', '-h', GetoptLong::NO_ARGUMENT ]
+    [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+    ['-f', GetoptLong::OPTIONAL_ARGUMENT],
+    ['-u', GetoptLong::OPTIONAL_ARGUMENT]
   )
-  opts.each do |opt|
+  scp_hostname = nil
+  scp_username = nil
+  opts.each do |opt, arg|
     case opt
     when '--help'
       puts <<-EOF
@@ -17,41 +22,71 @@ This program unpacks all tarballs, changes their names, uses the split-supportco
 
 -h, --help:
    show help
+-f HOSTADRESS:
+   fetches files from host
+-u USERNAME:
+   requires a username
 
       EOF
-    #else
-    #  run_task
+    when '-f'
+      scp_hostname = arg.to_s
+    when '-u'
+      scp_username = arg.to_s
+      Net::SFTP.start( scp_hostname, scp_username ) do |sftp|
+        sftp.dir.glob("/#{scp_username}/varlog", "nts_*.tbz") do |file|
+          sftp.download!( "/#{scp_username}/varlog/#{file.name}", "./#{file.name}" )
+        end
+      end
+      tarballs
     end      
   end
-  
 end
-def arg_or_not
-if ARGV.empty?
-  Dir.glob("*.tbz"){|x| `tar -xf #{x}`}
+
+
+def new_name
   Dir.glob("*crowbar*[^a-z]"){|x|File.rename(x,"c")}
   count = 1
   Dir["*[0-9]"].each do |x|
     File.rename(x, count.to_s)
     count += 1
   end
-  Dir.chdir("./c") do
-    `split-supportconfig *.txt`
+end
+
+def file_split
+  `split-supportconfig *.txt`
     Dir.glob("*.txt"){|x| File.delete(x)}
     FileUtils.cp_r Dir.glob("rootfs/*"), "."
     FileUtils.rm_r "./rootfs"
-  end
-  Dir["[0-9]"].each do |num|
-    Dir.chdir(num) do
-      `split-supportconfig *.txt`
-      Dir.glob("*.txt"){|x| File.delete(x)}
-      FileUtils.cp_r Dir.glob("rootfs/*"), "."
-      FileUtils.rm_r "./rootfs"
-    end
-  end
-else
-  argument
-end
 end
 
-arg_or_not
+def tarballs
+   Dir.glob("*.tbz"){|x| `tar -xf #{x}`}
+    new_name
+    Dir.chdir("./c") do
+      file_split
+    end
+    Dir["[0-9]"].each do |num|
+      Dir.chdir(num) do
+        file_split
+      end
+    end
+end
+
+
+
+def arg_choose
+  if ARGV.include? '-h'
+    argument
+  elsif ARGV.include? '--help'
+    argument
+  elsif ARGV.include? '-f'
+    argument
+  else
+    tarballs
+  end
+end
+
+arg_choose
+
+
 
